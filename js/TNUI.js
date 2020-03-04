ca390112e0606cb7866c85c9ff2685b17ca730af
banisle <banisle@gmail.com>
2020-03-04 11:39:09 +0900

    flow select 충돌 merge

--- js/TNUI.js
+++ js/TNUI.js
@@ -2558,52 +2558,6 @@ TNUI.module = (function () {
             });
         },
 
-<<<<<<< HEAD
-        //mark: flowSelect
-
-        // flowSelect : function(){
-        //     init = function(){
-        //         $('li').on('click',function(){
-        //             var $this = $(this);
-        //             flowSelect.mov($this);
-        //         });
-        //     },
-        //     mov = function($this){
-        //         var speed = 100;
-
-        //         var loop = setInterval(function(){
-        //             var liOn = $this.siblings().filter('.on'),
-        //                 destNum = parseInt($this.text()),
-        //                 curNum = parseInt(liOn.children().text());
-        //             var direction = Math.sign(destNum - curNum);//위아래인지 방향만
-        //             // console.log(test);
-        //             liOn.removeClass('on');
-        //             if(direction >= 1){//높은층으로 //prev
-        //                 console.log('+', 'curNum',curNum, 'destNum', destNum);
-        //                 liOn.prev().addClass('on');
-        //                 if(curNum +1 >= destNum){
-        //                     clearInterval(loop);
-        //                     console.log('end');
-        //                 }
-        //             }else if(direction < 0){//낮은층으로 //next
-        //                 console.log('-','curNum',curNum, 'destNum', destNum);
-        //                 liOn.next().addClass('on');
-        //                 if(curNum -1 <= destNum){
-        //                     clearInterval(loop);
-        //                     console.log('end');
-        //                 } 
-        //             }else{//같은 층 누르면
-        //                 clearInterval(loop);
-        //                 // flowSelect.stop();
-        //             }
-        //             console.log('curNum',curNum, 'destNum', destNum,direction);
-        //         },speed);
-        //     },
-        //     stop = function(){
-        //         clearInterval(loop);
-        //     }
-        // },
-=======
         flowSelect : function() {
               $(document).on('click','.flow-list li', function(){
                     var $this = $(this);
@@ -2642,7 +2596,6 @@ TNUI.module = (function () {
             }
 
         },
->>>>>>> 8f5a66d06ec87bcbb1c87503bf9341cddc634967
         
         // mark : init 
         init: function () {
