66f73c530deac2e2ee045c1f9c59308f94b311fa
banisle <banisle@gmail.com>
2020-04-08 16:45:24 +0900

    검색 서브 메뉴 삭제

--- js/TNUI.js
+++ js/TNUI.js
@@ -2056,34 +2056,25 @@ TNUI.module = (function () {
             var inpTxtDel = function () {
                 var $inp_form = $('.ui-hasDelForm'),
                     $inp = $inp_form.find('input.ui-hasDel'),
-                    $btnDel = $('.ui-deltxt'),
-                    isOpen = false;
+                    $btnDel = $('.ui-deltxt');
                 
                 // 삭제 버튼 생성
                 var createDelbtn = function(e){
-                    var $btn = '<button type="button" class="ui-deltxt" title="내용 삭제"></button>',
+                    var $btn = '<button type="button" class="ui-deltxt hidden" title="내용 삭제"></button>',
                         $this = $(e);
 
                     $this.append($btn);
                 }
                 createDelbtn($inp_form);
 
-                $inp.on('propertychange change keyup paste input focusin', function (e) {
+                $inp.on('input propertychange', function (e) {
                     var t = $(e.target),
-                        curVal = t.val(),
-                        oldVal,
+                        visible = Boolean(t.val()),
                         $btnDel = $('.ui-deltxt');
+
                     
-                    if (curVal !== oldVal && curVal !== '') {
-                        t.closest($inp_form).find($btnDel).addClass('active');
-                        isOpen = true;
-                        return;
-                    } else {
-                        t.closest($inp_form).find($btnDel).removeClass('active');
-                        isOpen = false;
-                    }
-                    oldVal = curVal;
-                });
+                    t.closest($inp_form).find($btnDel).toggleClass('hidden', !visible);
+                }).trigger('propertychange');
 
                 //포커스 잃었을때 
                 // $(document).on('focusin click', function (e) {
@@ -2100,10 +2091,8 @@ TNUI.module = (function () {
                 $(document).on('click',$btnDel, function (e) {
 
                     var $t = $(e.target);
+                    $t.prev($inp).val('').trigger('propertychange').focus();
                    
-                    $t.prev($inp).val('').removeClass('active');
-                    $t.removeClass('active');
-                    isOpen = false;
                 });
             }();
             console.log('inputDelUI');
